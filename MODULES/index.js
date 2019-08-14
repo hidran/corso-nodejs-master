74a66d53c71838ea2d9fd1ad830a2965ea140680
Hidran Arias <hidran@gmail.com>
2019-08-14 02:20:00 +0200

    explains require for native modules, .json files and how node searches for required files

--- MODULES/index.js
+++ MODULES/index.js
@@ -1,5 +1,16 @@
 const OPS = require('./functions'); // functions.js | functions.node
 
-//console.log(name);
+const config = require('./config');
+// node searches in its module
+// in a folder named node_modules
+// $HOME/node_modules
+// $HOME/node_libraries
+//  $NODE_PATH = D:/MODULES;
+console.log(config);
+// native module fs
+const fs = require('fs');
+// reads json file
+const api = require('./api.json');
+console.log(api);
 console.log(OPS);
 console.log(OPS.calcRectArea(6,6))
