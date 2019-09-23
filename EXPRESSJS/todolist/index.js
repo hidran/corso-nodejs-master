1b09ea41d81101884a16343c47b49c44d31c457e
Hidran <hidran@gmail.com>
2019-09-24 00:45:58 +0200

    Adss bootstrap to main layout

--- EXPRESSJS/todolist/index.js
+++ EXPRESSJS/todolist/index.js
@@ -3,6 +3,7 @@ const app = express();
 // C R U D
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 const ehb = require('express-handlebars');
 
 app.engine('.hbs', ehb({extname:'.hbs'}));
