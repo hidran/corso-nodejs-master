290208ed255873243a8705fe879171c4a243b858
Hidran <hidran@gmail.com>
2019-09-24 00:02:10 +0200

    adds express-handlebars and configures it

--- EXPRESSJS/todolist/index.js
+++ EXPRESSJS/todolist/index.js
@@ -3,11 +3,16 @@ const app = express();
 // C R U D
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-//app.use('/todos/:id',logger);
+const ehb = require('express-handlebars');
+
+app.engine('.hbs', ehb({extname:'.hbs'}));
+app.set('view engine','.hbs');
 const todosRoutes  = require('./routes/todos');
 const listsRoutes  = require('./routes/lists');
 
 app.use('/todos', todosRoutes);
 app.use('/lists',listsRoutes );
-
+app.get('/',(req,res) =>{
+    res.render('index');
+});
 app.listen(4000, ()=> console.log('listening on port 4000'));
